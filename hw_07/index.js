c4df5eabc0659eb688ea2c1e645b6437756cf5ec
Maxim Kilin <kilinyara05@gmail.com>
2022-10-31 19:27:04 +0200

    hw_07

--- hw_07/index.js
+++ hw_07/index.js
@@ -1,13 +1,13 @@
 let age = prompt("What year are you born?");
-let born;
-1
+let born = ("");
+
 if(age)
     {born = (`${"You're"} ${2022-age} ${"years old now ."}`)} 
     else 
         {alert("It is a pity that you did not want to enter your year of birth")};
 
 let city = prompt("what city do you live?");
-let live;
+let live = ("");
 
 if(city)
     {live = (`${"You live in"} ${city} ${"."}`)}
@@ -15,7 +15,7 @@ if(city)
         {alert("It is a pity that you did not want to enter your city")};
 
 let sport = prompt("what is your favorite sport?");
-let sportType;
+let sportType = ("");
 
 
 if(sport){
