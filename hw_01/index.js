b9ded0b7eb569d10cfbe06b39e1b60525442510a
Maxim Kilin <kilinyara05@gmail.com>
2022-11-24 13:42:42 +0200

    hw_13

--- hw_01/index.js
+++ hw_01/index.js
@@ -1,2 +1,3 @@
 var b;
 console.log ("b = ", b);
+
--- hw_13/index.js
+++ hw_13/index.js
@@ -1,11 +1,12 @@
 // Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
 
-
-
 const array = [1, 2, 3, 4, 5, 6, 7];
 function removeElement(arr,elem){
     return arr.filter((item) => item !== elem)
 }
 console.log(removeElement(array, 5));
+
+
+
 
