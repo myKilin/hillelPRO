e6d17136b0ba0deac9af17f021443a5a6a770d26
Maxim Kilin <kilinyara05@gmail.com>
2022-12-20 03:55:56 +0200

    hw_22

---
+++ hw_22/index.js
@@ -0,0 +1,23 @@
+// Вивести на сторінку таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється за допомогою JavaScript).
+
+const container = document.querySelector(".container");
+const table = document.createElement("table");
+container.appendChild(table);
+
+let y = 0;
+let colums = 10;
+let rows = 10;
+function createTable (colums, rows) {
+   for (let i = 1; i <= rows; i++){
+      let tr = document.createElement('tr');
+      for(let j = 1; j <= colums; j++) {
+         let td = document.createElement('td');
+         y+=1;
+         td.innerText = y;
+         tr.appendChild(td)
+      }
+      table.appendChild(tr)
+   }
+}
+
+createTable(colums,rows)
